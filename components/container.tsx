41fa9120fc015ab97ebebd6f2c712ca5b7503415
r-ushil <rushil.patel791@gmail.com>
2022-10-09 17:51:08 +0100

    feat: implemented container component

---
+++ components/container.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { chakra, Divider, Flex, Stack, Text, VStack } from '@chakra-ui/react'
+import Navbar from './navbar'
+
+
+interface Props {
+    children: React.ReactNode
+}
+
+export default function Container({children}: Props) {
+
+  return (
+    <>
+      <Navbar/>
+      <Divider />
+      <Flex as="main" justifyContent="center" flexDirection="column">
+        {children}
+      </Flex>
+      <Stack alignItems="center" mt={10} mb={5}>
+        <Text textAlign="center" fontSize={["0.5em", "sm"]} color="displayColor">
+          Designed and Developed by *YOUR NAME*.
+          <br />
+          Built with{' '}
+          <chakra.span fontWeight="semibold" color="button1">
+            Next.js
+          </chakra.span>
+          . Hosted on{' '}
+          <chakra.span fontWeight="semibold" color="button1">
+            Vercel
+          </chakra.span>
+          .
+        </Text>
+      </Stack>
+    </>
+  )
+}

feea4f4757b2be175ba79cfea5f5479f0de601d5
r-ushil <rushil.patel791@gmail.com>
2022-10-09 17:50:55 +0100

    feat: added navbar component

---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+import { Box, Button, Flex, Spacer, Text } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import NextLink from "next/link";
+
+export default function Navbar() {
+  const Bracket = styled.span`
+    color: #8f9094;
+    font-weight: 600;
+  `;
+
+
+  return (
+    <Flex minWidth='max-content' justifyContent="center" alignItems="center" pl={"1vw"} pr={"2vw"}>
+      <Box p='2'>
+        <Text
+          cursor="pointer"
+          color="displayColor"
+          fontWeight="bold"
+          fontSize="32px">
+          <Bracket>&#123;</Bracket>GDSC<Bracket>&#125;</Bracket>
+        </Text>
+      </Box>
+      <Spacer />
+      <Box >
+        <NextLink href="/" passHref>
+          <Button as="a" variant="ghost" p="4" ml="3vw" fontSize="16px" color="textSecondary">
+            Home
+          </Button>
+        </NextLink>
+        <NextLink href="/todo!" passHref>
+          <Button as="a" variant="ghost" p="4" ml="3vw" fontSize="16px" color="textSecondary">
+            CV
+          </Button>
+        </NextLink>
+      </Box>
+    </Flex>
+  )
+}
